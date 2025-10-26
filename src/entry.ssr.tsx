144f065417e5528b6dcb5d0e35e23d0e8c0d7e0f
lethdat <imdat2999@gmail.com>
2025-10-26 16:29:16 +0700

    fix tabsize

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -1,26 +1,34 @@
 import { Context, Next } from "hono";
+import { createElement as _c } from "react";
 import { renderToReadableStream } from "react-dom/server";
-import { createStaticHandler, createStaticRouter, StaticRouterProvider } from "react-router";
+import {
+	createStaticHandler,
+	createStaticRouter,
+	StaticRouterProvider,
+} from "react-router";
+import { bootstrapModules } from "virtual:ssr-assets";
 import routes from "./routes";
 
 export const renderer = async (c: Context, next: Next) => {
-    const ssrAssets = await import("virtual:ssr-assets");
-    const { query, dataRoutes } = createStaticHandler(routes);
-    const context = await query(c.req.raw);
-    if (context instanceof Response) {
-        return context;
-    }
-    const router = createStaticRouter(dataRoutes, context);
-    const html = await renderToReadableStream(<StaticRouterProvider
-        router={router}
-        context={context}
-        nonce="the-nonce"
-    />, {
-        bootstrapModules: ssrAssets.bootstrapModules,
-    })
-    return new Response(html, {
-        headers: {
-            "content-type": "text/html;charset=utf-8",
-        },
-    });
+	const { query, dataRoutes } = createStaticHandler(routes);
+	const context = await query(c.req.raw);
+	if (context instanceof Response) {
+		return context;
+	}
+	const ahihi = 12;
+	const nonce = "the-nonce";
+	const router = createStaticRouter(dataRoutes, context);
+	const html = await renderToReadableStream(
+		_c(StaticRouterProvider, { context, router, nonce }),
+		{
+			bootstrapModules,
+			nonce,
+		},
+	);
+	return new Response(html, {
+		headers: {
+			"content-type": "text/html;charset=utf-8",
+			"Content-Encoding": "Identity",
+		},
+	});
 };
--- src/routes/root/index.tsx
+++ src/routes/root/index.tsx
@@ -1,11 +1,10 @@
-import { Link } from 'assetLink'
 import React, { PropsWithChildren } from 'react'
 import { Outlet } from 'react-router'
 const Root: React.FC = () => {
   return (
     <html>
       <head>
-        <Link rel="stylesheet" href="virtual:browser-entry#css" />
+        <link rel="stylesheet" href="/assets/index.css" />
       </head>
       <body><Outlet/></body>
     </html>
